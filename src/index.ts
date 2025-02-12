86f8596ee3a57594936ab2c77b0793888a9a3788
mateuszm-arianelabs <mateusz.marcinkowski@arianelabs.com>
2025-02-12 12:21:50 +0100

    fix: export utils and types to be accessible in npm package (CCS-110) (#18)
    Signed-off-by: mateuszm-arianelabs <mateusz.marcinkowski@arianelabs.com>

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import HederaAgentKit from './agent'
 import { createHederaTools } from './langchain'
 export * as tools from './langchain'
+export * as apiUtils from './utils/api-utils';
+export * as htsUtils from './utils/hts-format-utils';
+export * from './types';
 export { HederaAgentKit, createHederaTools } 
